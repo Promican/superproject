6f702fffc7a2f60113f71654b8934d52da99d41e
Promican <nacimican12@gmail.com>
2023-07-30 00:24:13 -0400

    back to 0... maybe not :)
    From SuperSimple2DKit-master

---
+++ Assets/Scripts/Parallax/ParallaxController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Finds all of the gameObjects that have a ParallaxLayer.cs script, and moves them!*/
+
+public class ParallaxController : MonoBehaviour
+{
+    public delegate void ParallaxCameraDelegate(float cameraPositionChangeX, float cameraPositionChangeY);
+    public ParallaxCameraDelegate onCameraMove;
+    private Vector2 oldCameraPosition;
+    List<ParallaxLayer> parallaxLayers = new List<ParallaxLayer>();
+
+    Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+        onCameraMove += MoveLayer;
+        FindLayers();
+        oldCameraPosition.x = cam.transform.position.x;
+        oldCameraPosition.y = cam.transform.position.y;
+    }
+
+    private void FixedUpdate()
+    {
+        if (cam.transform.position.x != oldCameraPosition.x || (cam.transform.position.y) != oldCameraPosition.y)
+        {
+            if (onCameraMove != null)
+            {
+                Vector2 cameraPositionChange;
+                cameraPositionChange = new Vector2(oldCameraPosition.x - cam.transform.position.x, oldCameraPosition.y - cam.transform.position.y);
+                onCameraMove(cameraPositionChange.x, cameraPositionChange.y);
+            }
+
+            oldCameraPosition = new Vector2(cam.transform.position.x, cam.transform.position.y);
+        }
+    }
+
+    //Finds all the objects that have a ParallaxLayer component, and adds them to the parallaxLayers list.
+    void FindLayers()
+    {
+        parallaxLayers.Clear();
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            ParallaxLayer layer = transform.GetChild(i).GetComponent<ParallaxLayer>();
+
+            if (layer != null)
+            {
+                parallaxLayers.Add(layer);
+            }
+        }
+    }
+
+    //Move each layer based on each layers position. This is being used via the ParallaxLayer script
+    void MoveLayer(float positionChangeX, float positionChangeY)
+    {
+        foreach (ParallaxLayer layer in parallaxLayers)
+        {
+            layer.MoveLayer(positionChangeX, positionChangeY);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# superproject
+# SuperSimple2DKit
+A simple kit meant to help jump start the creation of your 2D Unity game!
+
+Creator: Thomas Brush
+Install & Usage Instructions: https://www.youtube.com/watch?v=U-mwooH1Bro
+Unity Version: 2020.3.19f1
